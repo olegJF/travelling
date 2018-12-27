705d61a5d6e9c534611cd979d28e32eda9d6be0e
oleg <jf2@ua.fm>
2018-12-27 17:43:20 +0200

    Добавил приложение Города

---
+++ cities/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CitiesConfig(AppConfig):
+    name = 'cities'
+    verbose_name='Список городов'
---
+++ cities/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class City(models.Model):
+    name = models.CharField(max_length=100, unique=True, verbose_name='Город')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Город'
+        verbose_name_plural = 'Города'
+        ordering = ['name']
