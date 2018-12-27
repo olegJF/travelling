676f3d90cfbab11028e3f39f0f40c57521fa4c70
oleg <jf2@ua.fm>
2018-12-27 18:30:45 +0200

    Добавил функцию отображения для списка городов

--- cities/views.py
+++ cities/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import City
 
-# Create your views here.
+def home(request):
+    cities = City.objects.all()
+    return render(request, 'cities/home.html', {'objects_list': cities})
