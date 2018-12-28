9c1bb8f3a5fa964f7d34c5df0fd808d67e5c0ec6
oleg <jf2@ua.fm>
2018-12-28 12:03:58 +0200

    Отображение на основе класса, детализация записи Город.

--- cities/urls.py
+++ cities/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home
+from .views import home, CityDetailView
 
 urlpatterns = [
+    path('detail/<int:pk>/', CityDetailView.as_view(), name='detail'),
     path('', home, name='home'),
 
 ]
--- cities/views.py
+++ cities/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from django.views.generic.detail import DetailView
 from .models import City
 
 def home(request):
     cities = City.objects.all()
     return render(request, 'cities/home.html', {'objects_list': cities})
+
+
+class CityDetailView(DetailView):
+    queryset = City.objects.all()
+    context_object_name = 'object'
+    template_name = 'cities/detail.html'
+    
