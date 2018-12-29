09acb27932260d6b010c52d5d80138f0854cee8a
oleg <jf2@ua.fm>
2018-12-29 11:35:12 +0200

    Редактирование и удаление записей города

--- cities/views.py
+++ cities/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
 from .models import City
 from .forms import CityForm
