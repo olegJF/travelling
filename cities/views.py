b3eb99ce2139465630e954f94a28cff3a62abe29
oleg <jf2@ua.fm>
2018-12-29 12:48:11 +0200

    Добавил пагинацию

--- cities/views.py
+++ cities/views.py
@@ -1,21 +1,25 @@
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.core.paginator import Paginator
 from django.urls import reverse_lazy
 from .models import City
 from .forms import CityForm
 
 def home(request):
-    if request.method == 'POST':
-        form = CityForm(request.POST or None)
-        if form.is_valid():
-            print(form.cleaned_data)
-    form = CityForm()
-    # print(request.POST)
-    city = request.POST.get('name')
-    # print(city)
+    # if request.method == 'POST':
+    #     form = CityForm(request.POST or None)
+    #     if form.is_valid():
+    #         print(form.cleaned_data)
+    # form = CityForm()
+    # # print(request.POST)
+    # city = request.POST.get('name')
+    # # print(city)
     cities = City.objects.all()
-    return render(request, 'cities/home.html', {'objects_list': cities, 'form': form})
+    paginator = Paginator(cities, 2)
+    page = request.GET.get('page')
+    cities = paginator.get_page(page)
+    return render(request, 'cities/home.html', {'objects_list': cities, })
 
 
 class CityDetailView(DetailView):
