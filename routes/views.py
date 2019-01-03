fcbba552533759964191ccfd3ef44da77284ec8a
oleg <jf2@ua.fm>
2019-01-03 15:15:19 +0200

    добавил функции по созданию графа и поиска путей в графе

--- routes/views.py
+++ routes/views.py
@@ -1,6 +1,34 @@
 from django.shortcuts import render
 from django.contrib import messages
+from trains.models import Train
 from .forms import *
+
+def dfs_paths(graph, start, goal):
+    """Функция поиска всех возможных маршрутов
+       из одного города в другой. Вариант посещения
+       одного и того же города более одного раза,
+        не рассматривается. 
+    """
+    stack = [(start, [start])]
+    while stack:
+        (vertex, path) = stack.pop()
+        if vertex in graph.keys():
+            for next_ in graph[vertex] - set(path):
+                if next_ == goal:
+                    yield path + [next_]
+                else:
+                    stack.append((next_, path + [next_]))
+
+def get_graph():
+    qs = Train.objects.values('from_city')
+    from_city_set = set(i['from_city'] for i in qs)
+    graph = {}
+    for city in from_city_set:
+        trains = Train.objects.filter(from_city=city).values('to_city')
+        tmp = set(i['to_city'] for i in trains)
+        graph[city] = tmp
+    
+    return graph
 
 def home(request):
     form = RouteForm()
@@ -11,7 +39,7 @@ def find_routes(request):
         form = RouteForm(request.POST or None)
         if form.is_valid():
             data = form.changed_data
-            assert False
+            # assert False
         return render(request, 'routes/home.html', {'form': form})
     else:
         messages.error(request, 'Создайте маршрут')
