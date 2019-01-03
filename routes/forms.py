d4183f9cde42c4c65e91f7608b4c5835ffd83ab3
oleg <jf2@ua.fm>
2019-01-03 13:09:38 +0200

    добавил скрипт Select2

--- routes/forms.py
+++ routes/forms.py
@@ -3,12 +3,12 @@ from cities.models import City
 
 class RouteForm(forms.Form):
     from_city = forms.ModelChoiceField(label='Откуда', queryset=City.objects.all(),
-                            widget=forms.Select(attrs={'class': 'form-control'}))
+                            widget=forms.Select(attrs={'class': 'form-control js-example-basic-single'}))
     to_city = forms.ModelChoiceField(label='Куда', queryset=City.objects.all(),
-                            widget=forms.Select(attrs={'class': 'form-control'}))
+                            widget=forms.Select(attrs={'class': 'form-control js-example-basic-single'}))
     across_cities = forms.ModelMultipleChoiceField(label='Через города', queryset=City.objects.all(),
                                 required=False,
-                                widget=forms.Select(attrs={'class': 'form-control'}))
+                                widget=forms.SelectMultiple(attrs={'class': 'form-control js-example-basic-multiple'}))
     traveling_time = forms.IntegerField(label='Поезд', 
                             widget=forms.NumberInput(
                                 attrs={'class': 'form-control',
