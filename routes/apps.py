0db69023c10cb20f696e17f0a6afda3c2bb0361d
oleg <jf2@ua.fm>
2019-01-03 12:07:20 +0200

    Новое приложение Маршруты

---
+++ routes/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RoutesConfig(AppConfig):
+    name = 'routes'
---
+++ routes/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from trains.models import Train
+
+
+class Route(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Название маршрута', unique=True)
+    from_city = models.CharField(max_length=100, verbose_name='Откуда')
+    to_city = models.CharField(max_length=100, verbose_name='Куда')
+    across_cities = models.ManyToManyField(Train, blank=True, verbose_name='Через города')
+    travel_times = models.IntegerField(verbose_name='Время в пути')
+
+    def __str__(self):
+        return self.name
+
+    
+    class Meta:
+        verbose_name = 'Маршрут'
+        verbose_name_plural = 'Маршруты'
+        ordering = ['name']
