3f0933e973e94d192bb8d53021e0dbfdb5ceda60
oleg <jf2@ua.fm>
2019-01-04 17:13:59 +0200

    Кастомизация отображения данных в админке

--- trains/admin.py
+++ trains/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Train
 
-admin.site.register(Train)
+class TrainAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Train
+    list_display = ('name', 'from_city', 'to_city', 'travel_time')
+    list_editable = ['travel_time']
+
+admin.site.register(Train, TrainAdmin)
