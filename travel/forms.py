2072b49662f41ef3a642f4563a94dd1c2cb24f2c
Oleg Novikov <jf2@ua.fm>
2020-10-04 09:57:59 +0300

    Обновил проверку пользователя в форме входа.
    Проверка всех атрибутов - юзернайма, пароля и активен ли пользователь.

--- travel/forms.py
+++ travel/forms.py
@@ -6,17 +6,19 @@ User = get_user_model()
 class UserLoginForm(forms.Form):
     username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
-
+    
     def clean(self, *args, **kwargs):
-        username = self.cleaned_data.get('username')
-        password = self.cleaned_data.get('password')
-
+        username = self.cleaned_data.get('username').strip()
+        password = self.cleaned_data.get('password').strip()
         if username and password:
-            user = authenticate(username=username, password=password)
-            if not user:
-                raise forms.ValidationError('Такого пользователя нет')
-            if not user.check_password(password):
-                raise forms.ValidationError('Неверный пароль')
+            qs = User.objects.filter(username=username)
+        if not qs.exists():
+            raise forms.ValidationError('Такого пользователя нет!')
+        if not check_password(password, qs[0].password):
+            raise forms.ValidationError('Пароль неверный!')
+        user = authenticate(username=username, password=password)
+        if not user:
+            raise forms.ValidationError('Данный аккаунт отключен')
         return super(UserLoginForm, self).clean(*args, **kwargs)
 
 
